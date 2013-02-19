5895c2196e3d9b28333b8129ea4995611aed616f
faust45 <faust451@gmail.com>
2013-02-19 22:52:33 +0200

    simplify things

--- _attachments/js/save_data.js
+++ _attachments/js/save_data.js
@@ -1,6 +1,4 @@
 //Save to bookmark: javascript:void((function(d){var%20e=d.createElement('script');e.setAttribute('type','text/javascript');e.setAttribute('charset','UTF-8');e.setAttribute('src','//faust45.cloudant.com/data/_design/data/js/save_data.js?r='+Math.floor(Math.random()*99999999));d.body.appendChild(e)})(document));
-d=document;
-e=d.createElement('script');e.setAttribute('type','text/javascript');e.setAttribute('charset','UTF-8');e.setAttribute('src','//ajax.googleapis.com/ajax/libs/jqueryui/1.10.1/jquery-ui.min.js');d.body.appendChild(e);
 sendData();
 
 function sendData() {
@@ -12,24 +10,7 @@ function sendData() {
     yqlQuery = encodeURIComponent('use "http://faust45.cloudant.com/data/_design/data/tables/demo.xml"; select * from demo where url="' + url + '" and postdata="' + quote + '"');
     yqlURL = 'http://query.yahooapis.com/v1/public/yql?q=' + yqlQuery + '&format=json'
 
-    $.post(yqlURL, function(data) {
-      console.log(data);
-    });
+    var request = new XMLHttpRequest();
+    request.open("GET", yqlURL);
+    request.send();
 }
-
-
-function parametersToString(parameters, encodeURI) {
-        var str = "",
-            key,
-            parameter;
-            
-        for (key in parameters) {
-            if (parameters.hasOwnProperty(key)) {
-                key = encodeURI ? encodeURIComponent(key) : key;
-                parameter = encodeURI ? encodeURIComponent(parameters[key]) : parameters[key];
-                str += key + "=" + parameter + "&";
-            }
-        }
-        return str.replace(/&$/, "");
-    };
-
